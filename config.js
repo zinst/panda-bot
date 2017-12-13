1dcbf69770e96440f5efb7e96efbebb190e39402
Tom Shaw <tommyo242@hotmail.com>
2017-12-13 22:45:04 +0000

    config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'BBNc5NlkkHKoa7XVyXSs8iKEb', 
+  consumer_secret:      'ONw9yHDjgAM2t7m8pru2v24apM3zJKV0i4WNjf5lyK3MIidCQ1',
+  access_token:         '940019915884462081-BgILrf1NmTS2VBSGCM0rXj4sgBpW8gF',
+  access_token_secret:  'QciBXKU2rByVm4EoJWV7FPorNDpg1U4GL08VsFeKfV1hL'
+}
